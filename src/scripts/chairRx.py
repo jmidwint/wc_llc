3db94aa0a234f2eb2f31546f084403cb0928ac0b
Jenny Midwinter <jenny@bluehorizon.ai>
2019-04-17 18:20:50 +0000

    Instrumented msg support from Arduino

--- src/scripts/chairRx.py
+++ src/scripts/chairRx.py
@@ -12,7 +12,7 @@ from std_msgs.msg import String
 
 from wc_msgs.msg import Chair
 
-
+JKM=True
 
 #ARDUINO = '/dev/ttyUSB0' #'/dev/ttyACM0' --- depending on the arduino hw
 
@@ -26,14 +26,31 @@ def chairRx():
     pub = rospy.Publisher('chair', Chair, queue_size=10)
     rospy.init_node('chairRx', anonymous=True)
 
-    msg = Chair()
+
     while True:
         # Wait until message arrives on serial port
         data = ser.readline().strip()
         # rospy.loginfo(rospy.get_caller_id() + " Received on serial port %s", data )
-        msg.header.stamp = rospy.Time.now()
-        msg.data.data = str(data)
-        pub.publish(msg)
+        #
+        # JKM: Klugy i/f ... for now we have a : deliminator 
+        # This should split our list into 2 
+        sentence = str(data)
+        [msgtype, msginfo] = sentence.split(":", 1)  # split into two parts
+
+        #if JKM: print("msgtype: ", msgtype)
+        #if JKM: print("msginfo: ", msginfo)
+        
+        # Publish to topic if its data or an echo
+        if( msgtype == "DATA" or msgtype == "ECHO" ):
+            msg = Chair()
+            msg.header.stamp = rospy.Time.now()
+            msg.data.data = str(msginfo)
+            pub.publish(msg)
+        else:
+            # Everything else gets logged as info for now.
+            # TODO: can further specify if an DEBUG, ERROR, RESPONSE 
+            rospy.loginfo(rospy.get_caller_id() + 
+                          " Received on serial port %s", msgtype + ":" + msginfo )
         #pub.publish(data)
     #rospy.spin()
 
